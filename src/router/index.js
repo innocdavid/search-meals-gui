d1e069a0d1ec1c654e3186f8e24ac1e7465053cb
Banja David Lotigo <innocdavid@gmail.com>
2023-05-21 13:14:42 +0300

    Add router file configuration

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path:'/',
+    name: 'home',
+    component: HomeComponent,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
