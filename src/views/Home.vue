03266b991333207525b48982cdbb0a5e21c746d2
Banja David Lotigo <innocdavid@gmail.com>
2023-05-21 14:13:10 +0300

    Add a route by letter feature for a meal list component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="flex flex-col p-8">
     <div>
-      <input type="text" class="rounded border-2 border-gray-200 w-full"
-      placeholder="Search for meals" />
+      <input 
+        type="text" 
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for meals" 
+      />
     </div>
 
     <div class="flex justify-center gap-2 mt-2">
-      <router-link to="/" v-for="letter of letters">
+      <router-link 
+        :to="{ name: 'byLetter', params: { letter } }" 
+        v-for="letter of letters" 
+        :key="letter"
+      >
         {{ letter  }}
       </router-link>
     </div>
@@ -18,6 +25,5 @@
 import { computed } from 'vue';
 import store from '../store';
 
-const meals = computed(() => store.state.meals);
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
 </script>
